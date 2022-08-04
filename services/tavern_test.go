7d790408a21cbf549e34043dd6cb37b055de2e43
Renato Spakauskas <renatospaka@gmail.com>
2022-08-04 09:07:38 -0300

    feat: :sparkles: tavern service
    tavern service

---
+++ services/tavern_test.go
@@ -0,0 +1,44 @@
+package services
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/renatospaka/tavern/aggregate"
+)
+
+func Test_Tavern(t *testing.T) {
+	// Create OrderService
+	products := init_products(t)
+
+	os, err := NewOrderService(
+		WithMemoryCustomerRepository(),
+		WithMemoryProductRepository(products),
+	)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tavern, err := NewTavern(WithOrderService(os))
+	if err != nil {
+		t.Error(err)
+	}
+
+	cust, err := aggregate.NewCustomer("Percy")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = os.customers.Add(cust)
+	if err != nil {
+		t.Error(err)
+	}
+	order := []uuid.UUID{
+		products[0].GetID(),
+	}
+
+	err = tavern.Order(cust.GetID(), order)
+	if err != nil {
+		t.Error(err)
+	}
+}
